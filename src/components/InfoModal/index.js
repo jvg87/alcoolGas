e6cc65abe5c83c1997c4ac04002b2b4916a1b44f
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-06-13 21:39:46 -0300

    style: finish modal style

--- src/components/InfoModal/index.js
+++ src/components/InfoModal/index.js
@@ -7,18 +7,18 @@ export default function InfoModal({ close }) {
       <Image
         source={require('../../images/gas.png')}
       />
-      <Text style={styles.title}>Conpensa usar Álcool</Text>
+      <Text style={styles.title}>Compensa usar Álcool</Text>
       <View style={styles.results}>
         <Text style={styles.tilteResults}>Com os preços:</Text>
         <Text style={styles.textResults}>Álcool: R$ 4.60</Text>
         <Text style={styles.textResults}>Gasolina: R$ 7.60</Text>
+        <TouchableOpacity 
+          style={styles.btn}
+          onPress={close}
+        >
+          <Text style={styles.btnText}>Calcular novamente</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity 
-        style={styles.btn}
-        onPress={close}
-      >
-        <Text style={styles.btnText}>Calcular novamente</Text>
-      </TouchableOpacity>
     </View>
   )
 }
@@ -28,33 +28,43 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent:'center',
     alignItems: 'center',
-    backgroundColor: '#212121'
+    backgroundColor: '#212121',
+    paddingHorizontal: 30
   },
   title: {
     color: '#2AD15E',
     fontSize: 30,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginVertical: 20
   },  
   results: {
+    flex:.25,
+    alignItems: 'center',
     width: '100%'
   },
   tilteResults: {
     color: '#fcfcfc',
     fontSize: 25,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginBottom: 20
   },  
   textResults: {
-    color: '#fcfcfc'
+    color: '#fcfcfc',
+    marginBottom: 20,
+    fontSize: 18,
   },
   btn: {
     width: '100%',
-    borderWidth: '#ff502f',
+    borderWidth:1,
+    borderColor: '#ff502f',
     borderRadius: 5,
-    marginTop: 20,
     padding: 10,
+    marginTop: 20
   },
   btnText: {
     textAlign: 'center',
-    color: '#ff502f'
+    color: '#ff502f',
+    fontSize: 18,
+    fontWeight: 'bold'
   }
 })
